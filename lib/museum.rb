613cbdeb92f84e9d08e8226b7f93dec2b27191aa
Alex M <Alexm717@hotmail.com>
2022-07-27 09:42:04 -0700

    methods for musem

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,2 +1,16 @@
 class Museum
+  attr_reader :name, :exhibits
+
+  def initialize(name)
+    @name = name
+    @exhibits = []
+  end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
+  end
+
+  def recommended_exhibits(patron)
+    @exhibits.select { |exhibit| patron.interests.include?(exhibit.name) }
+  end
 end
