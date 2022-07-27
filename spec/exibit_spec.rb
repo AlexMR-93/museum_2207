a7eeaca5c56b62f75351d846b454c6330cf514f3
Alex M <Alexm717@hotmail.com>
2022-07-27 08:26:07 -0700

    initial commit. create test for exhibit

---
+++ spec/exibit_spec.rb
@@ -0,0 +1,18 @@
+require "./lib/exhibit"
+require "rspec"
+
+
+describe(Exhibit) do
+  before(:each) do
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  it("exists") do
+    expect(@exhibit).to(be_an_instance_of(Exhibit))
+  end
+
+  it("attributes") do
+    expect(@exhibit.name).to(eq("Gems and Minerals"))
+    expect(@exhibit.cost).to(eq(0))
+  end
+end
